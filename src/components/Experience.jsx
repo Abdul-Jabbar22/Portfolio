5efc09aec8fe3c51317cdba5cf700678e6fa5281
Abdul-Jabbar22 <abduljabbarrar@example.com>
2025-05-05 10:38:54 +0500

    add new project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,24 +3,40 @@ import React from "react"
 
 const experiences = [
   {
+    company: "Self-Learning ",
+    position: "Web Developer (Self-Initiated Projects)",
+    period: "Jan 2025 to Continue",
+    description:
+      "Over 6 months of self-driven experience where I developed multiple full-stack web applications using the MERN stack. This includes real-world projects such as a Job Portal with JWT authentication, Role-Based Access Control, Event Management System, and a Portfolio website. Focused on writing scalable code, implementing best practices, and using modern frontend/backend tools.",
+    skills: [
+      "React.js", "Next.js", "Node.js", "Express.js", "MongoDB", "JWT",
+      "Tailwind CSS", "REST APIs", "Full-Stack Development", "Project Deployment", "Git", "Vercel", "Netlify"
+    ],
+  },
+  {
     company: "Txend",
-    position: "Internship",
+    position: "Internship – Paid",
     period: "Nov 2024 to Jan 2025",
-    description: "React development and SQA testing.",
-    skills: ["React", "Testing", "QA"],
+    description:
+      "3-month internship where I gained hands-on experience in React.js development and manual software testing (SQA). Contributed to a production-grade React.js project and participated in writing test cases using Jest and tracking progress with Jira.",
+    skills: [
+       "React.js", "Node.js", "Express.js", "MongoDB", "JWT", "Jest",
+      "Jira", "Software Testing", "QA"
+    ],
   },
   {
     company: "Future Dev Solutions",
     position: "Web Developer Internship",
     period: "Sep 2024 to Oct 2024",
-    description: "Worked with Next.js on dynamic web applications.",
+    description:
+      "Worked on dynamic web applications using Next.js and React.js, focusing on performance optimization and responsive UI components.",
     skills: ["Next.js", "React.js"],
   },
   {
     company: "TheWestral.com",
     position: "Order Manager",
     period: "Sep 2023 to Sep 2024",
-    description: "Order manager and operations (E-commerce).",
+    description: "Managed orders and operations in an e-commerce environment, ensuring timely fulfillment and smooth workflow.",
     skills: ["E-commerce", "Operations", "Management"],
   },
 ]
